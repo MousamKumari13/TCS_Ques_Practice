7e5cf15cba93f292e13a86c46f5ef99e5e905c66
Mousam kumari <133010326+MousamKumari13@users.noreply.github.com>
2024-12-12 10:42:38 +0000

    Find the nth series num

---
+++ Easy_question/01_tcs.cpp
@@ -0,0 +1,31 @@
+## Find  the 15th term of the series?
+``` 0,0,7,6,14,12,21,18, 28 ```
+Explanation :
+In this series the odd term is increment of  ``` 7 {0, 7, 14, 21, 28, 35 – – – – – – } ```
+And even term is a increment of  ```6 {0, 6, 12, 18, 24, 30 – – – – – – }```
+
+## input 
+``` 15 ```
+
+## output
+``` 42 ```
+
+# Solution 
+
+``` c++ 
+#include <bits/stdc++.h>
+using namespace std;
+int solve(int n){
+    if(n%2 == 0){
+         return 7* (n+1 /2);
+    }else{
+        return 6*(n/2);
+    }
+}
+int main() {
+   int n;
+   cin>>n;
+   cout<<solve(n)<<endl;
+    return 0;
+}
+``` 
